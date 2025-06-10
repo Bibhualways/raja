7285eb5e179ab041e5c9c47fd48d1cc4827e7868
Bibhualways <bibhutibhusanswain18@gmail.com>
2025-06-10 22:33:24 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-document.getElementById("download").addEventListener("click", function () {
-  html2canvas(document.querySelector("#card")).then(canvas => {
-    let link = document.createElement("a");
-    link.download = "rajo_wish.png";
-    link.href = canvas.toDataURL();
-    link.click();
-  });
-});
